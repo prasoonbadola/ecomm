7e990acd3463c15a7c0e073e0fff2f9655cc46b9
prasoonbadola@gmail.com <prasoonbadola@gmail.com>
2023-01-15 23:40:51 +0530

    changes

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express =  require('express');
+
+const app = express()
+
+const router = express.Router();
+
+const mongoose = require('mongoose');
+const serverless = require('serverless-http');
+
+mongoose.connect("mongodb+srv://prasoon:8561847387@cluster0.5huiky8.mongodb.net/?retryWrites=true&w=majority",
+{useNewUrlParser:true,
+useUnifiedTopology:true}).then(()=>{
+    console.log('DB connected..')
+});
+
+
+router.get('/',(req,res)=>{
+    res.send("kdasd");
+})
+
+app.use('/.netlify/functions/app',router)
+
+
+
+
+module.exports.handler = serverless(app);
